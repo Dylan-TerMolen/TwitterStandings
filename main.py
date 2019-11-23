ddd62fb009445adb23a37b37009134b1a7e5662f
Dylan-TerMolen <49906569+Dylan-TerMolen@users.noreply.github.com>
2019-11-22 21:26:51 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from Retriever import Retriever
 from datetime import date
 
 # API Key for the football-data.org API 
-api_key = "1f77263fcb0f4c94bd8aa7dbdee94b7c"
+api_key = "Use your credentials"
 
 # Twitter developer credentials for use of Twitter API 
-consumer_key = "wq9wxUWoDqnunVDIL0abJTrX0"
-consumer_secret = "ZuQmGvIKrr68bStxQPUiiVPGNzTmSpRsyVMvUYzlB28EpQJK0j"
-access_token = "1197269050574819328-GQot0H364WnPWAcdOaXKeod5tHuPwM"
-access_token_secret = "VetmFdTbDQTHGdGT8ZwZ3KmWnJ1cGRgEXiK0TDyGZIGwY"
+consumer_key = "Use your credentials"
+consumer_secret = "Use your credentials"
+access_token = "Use your credentials"
+access_token_secret = "Use your credentials"
 
 if __name__ == "__main__":
     myTweeter = Tweeter(consumer_key,consumer_secret,access_token,access_token_secret)
